c9455d722a93f378eca7376459d2adb81033a1f2
wakki <ytk0202@docomo.ne.jp>
2022-12-19 16:58:09 +0900

    done MV section SPver

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,37 +8,71 @@ export default function Home() {
   return (
     <div>
       <section className={MV.MVSection}>
-        <picture>
-          <img src="/Frame 9742.png" alt="" />
-        </picture>
-        
-        <div className={MV.cta_btn}>
-            <a href="/free-trial/" className={MV.box} >
-            <div>
-              <picture>
-                <img src="/Group 8261.png" alt="" className={MV.balloon} />
-              </picture>
-            </div>
-
-
-              <div className={MV.container}>
-                
-                <div>
-                  <picture>
-                    <img src="/Frame 9788.png" alt="" className={MV.BalloonImg} />
-                  </picture>
-                  </div>
-                <div>
-                  <picture>
-                    <img src="/無料相談に申し込む.png" alt="" className={MV.TextImg} />
-                  </picture>
-                </div>
-              
+        <div className={MV.PCver}>
+          <picture>
+            <img src="/Frame 9742.png" alt="" />
+          </picture>
+          
+          <div className={MV.CtaBtn}>
+              <a href="/free-trial/" className={MV.Box} >
+              <div>
+                <picture>
+                  <img src="/Group 8261.png" alt="" className={MV.Balloon} />
+                </picture>
               </div>
 
-              
-            </a>
-          </div>
+
+                <div className={MV.Container}>
+                  
+                  <div>
+                    <picture>
+                      <img src="/Frame 9788.png" alt="" className={MV.BalloonImg} />
+                    </picture>
+                    </div>
+                  <div>
+                    <picture>
+                      <img src="/無料相談に申し込む.png" alt="" className={MV.TextImg} />
+                    </picture>
+                  </div>
+                
+                </div>
+
+                
+              </a>
+            </div>
+        </div>
+        <div className={MV.SPver}>
+          <picture>
+            <img src="/Section_MV (1).png" alt="" />
+          </picture>
+          <div className={MV.CtaBtn}>
+              <a href="/free-trial/" className={MV.Box} >
+              <div>
+                <picture>
+                  <img src="/Group 8261.png" alt="" className={MV.Balloon} />
+                </picture>
+              </div>
+
+
+                <div className={MV.Container}>
+                  
+                  <div>
+                    <picture>
+                      <img src="/Frame 9788.png" alt="" className={MV.BalloonImg} />
+                    </picture>
+                    </div>
+                  <div>
+                    <picture>
+                      <img src="/無料相談に申し込む.png" alt="" className={MV.TextImg} />
+                    </picture>
+                  </div>
+                
+                </div>
+
+                
+              </a>
+            </div>
+        </div>
       </section>
     </div>
   );
