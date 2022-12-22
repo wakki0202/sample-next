033892e5adb99c02363ce3cc5f8db2bb409ae84f
wakki <ytk0202@docomo.ne.jp>
2022-12-22 12:23:13 +0900

    fix naming img&css_class_name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,61 +6,65 @@ import CtaSection from '../styles/module/CtaSection.module.scss';
 
 export default function Home() {
   return (
-   
+   <LayoutNew
+      header={<></>}
+      footer={<></>}
+    >
       <div>
-        <section className={CtaSection.CtaSection}>
+        <section className={CtaSection.cta_section}>
           <picture>
-          <img src="img/cta_section_heading.png" alt="" className={CtaSection.FreeConsultationImg} />
+          <img src="img/cta_section_heading.png" alt="" className={CtaSection.free_consultation_img} />
         </picture>
         
 
-        <div className={CtaSection.CtaBtn}>
-          <a className={CtaSection.CtaBtnLink}>
+        <div className={CtaSection.cta_btn}>
+          <a className={CtaSection.cta_btn_link}>
             <picture>
-              <img src="img/cta_section_baloon2.png" alt="" className={CtaSection.BaloonImg} />
+              <img src="img/cta_section_balloon.png" alt="" className={CtaSection.balloon_img} />
             </picture>
             <picture>
-              <img src="img/cta_section_men.png" alt="" className={CtaSection.MenImg}  />
+              <img src="img/cta_section_men.png" alt="" className={CtaSection.men_img}  />
             </picture>
             
-            <div className={CtaSection.CtnBtnContent}>
+            <div className={CtaSection.ctn_btn_content}>
               <picture>
-                <img src="img/mv_btn_txt.png" alt="" className={CtaSection.TxtImg}  />
+                <img src="img/cta_section_btn_txt.png" alt="" className={CtaSection.text_img}  />
               </picture>
               <picture>
-                <img src="img/Frame 9792.png" alt="" className={CtaSection.ArrowImg}  />
+                <img src="img/cta_section_arrow.png" alt="" className={CtaSection.arrow_img}  />
               </picture>
             </div>
           </a>
         </div>
-          <p className={CtaSection.FreeConsultationSurvey}>
+          <p className={CtaSection.free_consultation_survey}>
             ※ 無料相談実施後アンケートによる満足率(N=146)
             <br className={CtaSection.pc_none} />
             【2022年2月2日~2022年4月16日】
         </p>
         
-        <div className={CtaSection.CommentArea}>
-          <h3 className={CtaSection.CommentAreaTitle}>
+        <div className={CtaSection.comment_area}>
+          <h3 className={CtaSection.comment_area_title}>
             多数の方に無料体験のご満足を
             <br className={CtaSection.pc_none} />
             いただけています！
           </h3>
 
-          <div className={CtaSection.CommentImgList}>
+          <div className={CtaSection.comment_img_list}>
             <picture>
-              <img src="img/cta_section_comment_01.png" alt="" className={CtaSection.CommentImg} />
+              <img src="img/cta_section_comment_01.png" alt="" className={CtaSection.comment_img} />
             </picture>
 
             <picture>
-              <img src="img/cta_section_comment_02.png" alt="" className={CtaSection.CommentImg} />
+              <img src="img/cta_section_comment_02.png" alt="" className={CtaSection.comment_img} />
             </picture>
 
             <picture>
-              <img src="img/cta_section_comment_03.png" alt="" className={CtaSection.CommentImg}/>
+              <img src="img/cta_section_comment_03.png" alt="" className={CtaSection.comment_img}/>
             </picture>
           </div>
         </div>
         </section>
       </div>
+    </LayoutNew>
   );
 }
