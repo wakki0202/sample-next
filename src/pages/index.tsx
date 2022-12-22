b1a3c5ea837cd2395340bcad0880174abdea43f8
wakki <ytk0202@docomo.ne.jp>
2022-12-22 18:06:51 +0900

    fix css&image&css_class_name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,56 +8,120 @@ export default function Home() {
   return (
     <div>
 
-      <section className={RecommendSection.RecommendSection}>
-        <div className={RecommendSection.RecommendWrap}>
-          <div className={RecommendSection.PCStyle}>
+      <section className={RecommendSection.recommend_section}>
+        <div className={RecommendSection.recommend_wrap}>
+          <div className={RecommendSection.pc_style}>
             <picture>
-              <img src="/recommend_text_title_1440px.png" alt="" className={RecommendSection.RecommendTitleImg} />
+              <img src="/recommend_text_title_1440px.png" alt="" className={RecommendSection.recommend_title_img} />
             </picture>
 
-            <div className={RecommendSection.RecommendImgList}>
-              <picture>
-                <img src="/Frame 10297.png" alt="" className={
-                  RecommendSection.RecommendImg
-                } />
-              </picture>
-              
-              {/* <picture>
-                <img src="/Frame 10294.png" alt=""  className={RecommendSection.RecommendImg1} />
-              </picture>
+            <div className={RecommendSection.recommend_img_list}>
 
-              <picture>
-                <img src="/Frame 10295.png" alt=""  className={RecommendSection.RecommendImg2} />
-              </picture>
+              <ul className={RecommendSection.recommend_img_list1}>
 
-              <picture>
-                <img src="/Frame 10296.png" alt=""  className={RecommendSection.RecommendImg3} />
-              </picture> */}
+                <li className={RecommendSection.recommend_container}>
+                  <div className={RecommendSection.recommend_box}>
+                    <div className={RecommendSection.bg_black}>
+                      <p>IT企業に就職・インターンをしたい</p>
+                    </div>
+                    <div className={RecommendSection.recommend_text}>
+                      <p>・IT企業に興味があるけど、Webやプログラミングなどの業界知識がない</p>
+                      <p>・事前に業界知識を身に着けておきたい</p>
+                    </div>
+                  </div>
+                </li>
+                <li><img src="/recommend_polygon_right.svg" alt="" className={RecommendSection.recommend_polygon} /></li>
+                <li><img src="/recommend_image_1.png" alt="" className={RecommendSection.recommend_img} /></li>
 
+              </ul>
+
+              <ul className={RecommendSection.recommend_img_list2}>
+                <li>
+                  <div className={RecommendSection.recommend_box}>
+                    <div className={RecommendSection.bg_black}>
+                      <p>就活でアピールできる実績がほしい</p>
+                    </div>
+                    <div className={RecommendSection.recommend_text}>
+                      <p>・就職活動でエントリーシートに何を書いて良いかわからない</p>
+                      <p>・実績を積んで他の就活生に差をつけたい</p>
+                    </div>
+                  </div>
+                </li>
+                <li><img src="/recommend_polygon_right.svg" alt="" className={RecommendSection.recommend_polygon} /></li>
+                <li><img src="/recommend_image_2.png" alt="" className={RecommendSection.recommend_img} /></li>
+              </ul>
+
+              <ul className={RecommendSection.recommend_img_list3}>
+                <li>
+                  <div className={RecommendSection.recommend_box}>
+                    <div className={RecommendSection.bg_black}>
+                      <p>エンジニアになりたいけど<br/>何をしていいかわからない</p>
+                    </div>
+                    <div className={RecommendSection.recommend_text}>
+                      <p>・独学で挫折してしまい何から手をつけたら良いかわからない</p>
+                      <p>・相談できる人がいないので不安</p>
+                    </div>
+                  </div>
+                </li>
+                <li><img src="/recommend_polygon_right.svg" alt="" className={RecommendSection.recommend_polygon} /></li>
+                <li><img src="/recommend_image_3.png" alt="" className={RecommendSection.recommend_img} /></li>
+              </ul>
               
             </div>
           </div>
 
-          <div className={RecommendSection.SPStyle}>
+          <div className={RecommendSection.sp_style}>
             <picture>
-              <img src="/recommend_text_title_375px.png" alt="" className={RecommendSection.SPRecommendTitleImg} />
+              <img src="/recommend_text_title_375px.png" alt="" className={RecommendSection.sp_recommend_title_img} />
             </picture>
-            <div className={RecommendSection.SPRecommendImgList}>
-              <picture>
-                <img src="/Frame 10294 (1).png" alt="" className={
-                  RecommendSection.SPRecommendImg1
-                } />
-              </picture>
-              <picture>
-                <img src="/Frame 10295 (1).png" alt="" className={
-                  RecommendSection.SPRecommendImg2
-                } />
-              </picture>
-              <picture>
-                <img src="/Frame 10296 (1).png" alt="" className={
-                  RecommendSection.SPRecommendImg3
-                } />
-              </picture>
+            <div className={RecommendSection.sp_recommend_img_list}>
+              <ul className={RecommendSection.sp_recommend_img_list1}>
+                <li>
+                  <div className={RecommendSection.recommend_box}>
+                    <div className={RecommendSection.bg_black}>
+                      <p>IT企業に就職・インターンをしたい</p>
+                    </div>
+                    <div className={RecommendSection.recommend_text}>
+                      <p>・IT企業に興味があるけど、Webやプログラミングなどの業界知識がない</p>
+                      <p>・事前に業界知識を身に着けておきたい</p>
+                    </div>
+                  </div>
+                </li>
+                <li><img src="/recommend_polygon_bottom.png" alt="" className={RecommendSection.sp_recommend_polygon} /></li>
+                <li><img src="/recommend_image_1.png" alt="" className={RecommendSection.sp_recommend_img} /></li>
+              </ul>
+
+              <ul className={RecommendSection.sp_recommend_img_list2}>
+                <li>
+                  <div className={RecommendSection.recommend_box}>
+                    <div className={RecommendSection.bg_black}>
+                      <p>就活でアピールできる実績がほしい</p>
+                    </div>
+                    <div className={RecommendSection.recommend_text}>
+                      <p>・就職活動でエントリーシートに何を書いて良いかわからない</p>
+                      <p>・実績を積んで他の就活生に差をつけたい</p>
+                    </div>
+                  </div>
+                </li>
+                <li><img src="/recommend_polygon_bottom.png" alt="" className={RecommendSection.sp_recommend_polygon} /></li>
+                <li><img src="/recommend_image_2.png" alt="" className={RecommendSection.sp_recommend_img} /></li>
+              </ul>
+
+              <ul className={RecommendSection.sp_recommend_img_list3}>
+                <li>
+                  <div className={RecommendSection.recommend_box}>
+                    <div className={RecommendSection.bg_black}>
+                      <p>エンジニアになりたいけど<br/>何をしていいかわからない</p>
+                    </div>
+                    <div className={RecommendSection.recommend_text}>
+                      <p>・独学で挫折してしまい何から手をつけたら良いかわからない</p>
+                      <p>・相談できる人がいないので不安</p>
+                    </div>
+                  </div>
+                </li>
+                <li><img src="/recommend_polygon_bottom.png" alt="" className={RecommendSection.sp_recommend_polygon} /></li>
+                <li><img src="/recommend_image_3.png" alt="" className={RecommendSection.sp_recommend_img} /></li>
+              </ul>
             </div>
           </div>
         </div>
