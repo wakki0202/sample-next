9e2a37fcaae9f72101ef3141cd5433be84b80d27
wakki <ytk0202@docomo.ne.jp>
2023-01-05 14:39:52 +0900

    done picture削除&prettier導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Top.module.scss";
 import LayoutNew from "../layouts/LayoutNew/";
-import CampaignSection from 'src/styles/scss/module/CampaignSection.module.scss'
+import CampaignSection from "src/styles/scss/module/CampaignSection.module.scss";
 
 export default function Home() {
   return (
     <div>
       <section className={CampaignSection.CampaignSection}>
-        <picture>
-          <img src="Frame 9868.png" alt="" className={CampaignSection.CampaignTitleImg} />
-        </picture>
-        
+        <img
+          src="Frame 9868.png"
+          alt=""
+          className={CampaignSection.CampaignTitleImg}
+        />
       </section>
     </div>
   );
