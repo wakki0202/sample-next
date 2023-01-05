ad364e79d2d72bb66eb4d9b9d2ea4d13e8ec7d90
wakki <ytk0202@docomo.ne.jp>
2023-01-05 13:54:47 +0900

    done picture削除&&prettier導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,83 +2,55 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Top.module.scss";
 import LayoutNew from "../layouts/LayoutNew/";
-import MV from "../styles/scss/module/mv.module.scss"
+import MV from "../styles/scss/module/mv.module.scss";
 
 export default function Home() {
   return (
-    <LayoutNew
-      header={<></>}
-      footer={<></>}
-    >
-    <div>
-      <section className={MV.mv_section}>
-        <div className={MV.pc_ver}>
-          <picture>
+    <LayoutNew header={<></>} footer={<></>}>
+      <div>
+        <section className={MV.mv_section}>
+          <div className={MV.pc_ver}>
             <img src="/mv_pcver.png" alt="" />
-          </picture>
-          
-          <div className={MV.cta_btn}>
-              <a href="/free-trial/" className={MV.box} >
-              <div>
-                <picture>
+
+            <div className={MV.cta_btn}>
+              <a href="/free-trial/" className={MV.box}>
+                <div>
                   <img src="/balloon.png" alt="" className={MV.balloon} />
-                </picture>
-              </div>
-
-
-                <div className={MV.container}>
-                  
-                  <div>
-                    <picture>
-                      <img src="/arrow.png" alt="" className={MV.arrow} />
-                    </picture>
-                    </div>
-                  <div>
-                    <picture>
-                      <img src="/btn_text.png" alt="" className={MV.text_img} />
-                    </picture>
-                  </div>
-                
                 </div>
 
-                
+                <div className={MV.container}>
+                  <div>
+                    <img src="/arrow.png" alt="" className={MV.arrow} />
+                  </div>
+                  <div>
+                    <img src="/btn_text.png" alt="" className={MV.text_img} />
+                  </div>
+                </div>
               </a>
             </div>
-        </div>
-        <div className={MV.sp_ver}>
-          <picture>
+          </div>
+          <div className={MV.sp_ver}>
             <img src="/mv_spver.png" alt="" />
-          </picture>
-          <div className={MV.cta_btn}>
-              <a href="/free-trial/" className={MV.box} >
-              <div>
-                <picture>
+
+            <div className={MV.cta_btn}>
+              <a href="/free-trial/" className={MV.box}>
+                <div>
                   <img src="/balloon.png" alt="" className={MV.balloon} />
-                </picture>
-              </div>
-
-
-                <div className={MV.container}>
-                  
-                  <div>
-                    <picture>
-                      <img src="/arrow.png" alt="" className={MV.arrow} />
-                    </picture>
-                    </div>
-                  <div>
-                    <picture>
-                      <img src="/btn_text.png" alt="" className={MV.text_img} />
-                    </picture>
-                  </div>
-                
                 </div>
 
-                
+                <div className={MV.container}>
+                  <div>
+                    <img src="/arrow.png" alt="" className={MV.arrow} />
+                  </div>
+                  <div>
+                    <img src="/btn_text.png" alt="" className={MV.text_img} />
+                  </div>
+                </div>
               </a>
             </div>
-        </div>
-      </section>
+          </div>
+        </section>
       </div>
-      </LayoutNew>
+    </LayoutNew>
   );
 }
