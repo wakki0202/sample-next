203db56970a0d5e8d2d9153126633c2cce174eae
wakki <ytk0202@docomo.ne.jp>
2023-01-10 20:20:55 +0900

    done company-section spver

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,6 +50,9 @@ export default function Home() {
           </div>
 
           <div id="コメント" className={Company.recruiter_comment_overview}>
+            <div className={Company.recruiter_comment_highlight_sp}>
+              <img src="/company_comment.png" alt="" />
+            </div>
             <p>
               エンジニア職種にとどまらず、全てのビジネスマンにWebの知識は必須になっています。就活生のエントリーシートに、プログラミングを用いた個人の成果物があった場合、必ず目を通しますね。どの職種でもアドバンテージになると思います。TOEICだと数字でしか判断ができないのですが、個人ごとのWebの成果物の場合「なぜそのアプリケーションを作成したのか？」まで見ることができるため、ビジネス観点まで考察することができます。また、Webの知識を得た上でビジネスマンになることは、ポジティブでしかないと思います。
             </p>
@@ -57,10 +60,23 @@ export default function Home() {
 
           <div id="relative" className={Company.company_survey_result}>
             <div id="absolute balloon" className={Company.balloon_img}>
-              <img src="/company_balloon.png" alt="" />
+              <img
+                src="/company_balloon.png"
+                alt=""
+                className={Company.pc_ver}
+              />
+              <img
+                src="/company_balloon_sp.png"
+                alt=""
+                className={Company.sp_ver}
+              />
             </div>
             <h3>
-              就活で個人のWebの成果物がある場合、企業の97％が参考になると回答
+              就活で個人のWebの
+              <br className={Company.sp_ver} />
+              成果物がある場合、
+              <br className={Company.sp_ver} />
+              企業の97％が参考になると回答
             </h3>
           </div>
         </div>
