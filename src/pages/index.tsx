74f9e6bc53bcd77f0f46d58302f5fa9625f039b4
wakki <ytk0202@docomo.ne.jp>
2023-01-12 10:46:12 +0900

    fix scssファイル名をスネークケースに変更、css修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,31 +2,31 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Top.module.scss";
 import LayoutNew from "../layouts/LayoutNew/";
-import RecommendSection from "src/styles/scss/module/RecommendSection.module.scss";
+import recommend_section from "src/styles/scss/module/recommend_section.module.scss";
 
 export default function Home() {
   return (
     <LayoutNew header={<></>} footer={<></>}>
       <div>
-        <section className={RecommendSection.recommend_section}>
-          <div className={RecommendSection.recommend_wrap}>
-            <div className={RecommendSection.pc_style}>
+        <section className={recommend_section.recommend_section}>
+          <div className={recommend_section.recommend_wrap}>
+            <div className={recommend_section.pc_style}>
               <div>
                 <img
                   src="/recommend_text_title_1440px.png"
                   alt=""
-                  className={RecommendSection.recommend_title_img}
+                  className={recommend_section.recommend_title_img}
                 />
               </div>
 
-              <div className={RecommendSection.recommend_img_list}>
-                <ul className={RecommendSection.recommend_img_list1}>
-                  <li className={RecommendSection.recommend_container}>
-                    <div className={RecommendSection.recommend_box}>
-                      <div className={RecommendSection.bg_black}>
+              <div className={recommend_section.recommend_img_list}>
+                <ul className={recommend_section.recommend_img_list1}>
+                  <li className={recommend_section.recommend_container}>
+                    <div className={recommend_section.recommend_box}>
+                      <div className={recommend_section.bg_black}>
                         <p>IT企業に就職・インターンをしたい</p>
                       </div>
-                      <div className={RecommendSection.recommend_text}>
+                      <div className={recommend_section.recommend_text}>
                         <p>
                           ・IT企業に興味があるけど、Webやプログラミングなどの業界知識がない
                         </p>
@@ -38,25 +38,25 @@ export default function Home() {
                     <img
                       src="/recommend_polygon_right.svg"
                       alt=""
-                      className={RecommendSection.recommend_polygon}
+                      className={recommend_section.recommend_polygon}
                     />
                   </li>
                   <li>
                     <img
                       src="/recommend_image_1.png"
                       alt=""
-                      className={RecommendSection.recommend_img}
+                      className={recommend_section.recommend_img}
                     />
                   </li>
                 </ul>
 
-                <ul className={RecommendSection.recommend_img_list2}>
+                <ul className={recommend_section.recommend_img_list2}>
                   <li>
-                    <div className={RecommendSection.recommend_box}>
-                      <div className={RecommendSection.bg_black}>
+                    <div className={recommend_section.recommend_box}>
+                      <div className={recommend_section.bg_black}>
                         <p>就活でアピールできる実績がほしい</p>
                       </div>
-                      <div className={RecommendSection.recommend_text}>
+                      <div className={recommend_section.recommend_text}>
                         <p>
                           ・就職活動でエントリーシートに何を書いて良いかわからない
                         </p>
@@ -68,29 +68,29 @@ export default function Home() {
                     <img
                       src="/recommend_polygon_right.svg"
                       alt=""
-                      className={RecommendSection.recommend_polygon}
+                      className={recommend_section.recommend_polygon}
                     />
                   </li>
                   <li>
                     <img
                       src="/recommend_image_2.png"
                       alt=""
-                      className={RecommendSection.recommend_img}
+                      className={recommend_section.recommend_img}
                     />
                   </li>
                 </ul>
 
-                <ul className={RecommendSection.recommend_img_list3}>
+                <ul className={recommend_section.recommend_img_list3}>
                   <li>
-                    <div className={RecommendSection.recommend_box}>
-                      <div className={RecommendSection.bg_black}>
+                    <div className={recommend_section.recommend_box}>
+                      <div className={recommend_section.bg_black}>
                         <p>
                           エンジニアになりたいけど
                           <br />
                           何をしていいかわからない
                         </p>
                       </div>
-                      <div className={RecommendSection.recommend_text}>
+                      <div className={recommend_section.recommend_text}>
                         <p>
                           ・独学で挫折してしまい何から手をつけたら良いかわからない
                         </p>
@@ -102,36 +102,36 @@ export default function Home() {
                     <img
                       src="/recommend_polygon_right.svg"
                       alt=""
-                      className={RecommendSection.recommend_polygon}
+                      className={recommend_section.recommend_polygon}
                     />
                   </li>
                   <li>
                     <img
                       src="/recommend_image_3.png"
                       alt=""
-                      className={RecommendSection.recommend_img}
+                      className={recommend_section.recommend_img}
                     />
                   </li>
                 </ul>
               </div>
             </div>
 
-            <div className={RecommendSection.sp_style}>
+            <div className={recommend_section.sp_style}>
               <div>
                 <img
                   src="/recommend_text_title_375px.png"
                   alt=""
-                  className={RecommendSection.sp_recommend_title_img}
+                  className={recommend_section.sp_recommend_title_img}
                 />
               </div>
-              <div className={RecommendSection.sp_recommend_img_list}>
-                <ul className={RecommendSection.sp_recommend_img_list1}>
+              <div className={recommend_section.sp_recommend_img_list}>
+                <ul className={recommend_section.sp_recommend_img_list1}>
                   <li>
-                    <div className={RecommendSection.recommend_box}>
-                      <div className={RecommendSection.bg_black}>
+                    <div className={recommend_section.recommend_box}>
+                      <div className={recommend_section.bg_black}>
                         <p>IT企業に就職・インターンをしたい</p>
                       </div>
-                      <div className={RecommendSection.recommend_text}>
+                      <div className={recommend_section.recommend_text}>
                         <p>
                           ・IT企業に興味があるけど、Webやプログラミングなどの業界知識がない
                         </p>
@@ -143,25 +143,25 @@ export default function Home() {
                     <img
                       src="/recommend_polygon_bottom.png"
                       alt=""
-                      className={RecommendSection.sp_recommend_polygon}
+                      className={recommend_section.sp_recommend_polygon}
                     />
                   </li>
                   <li>
                     <img
                       src="/recommend_image_1.png"
                       alt=""
-                      className={RecommendSection.sp_recommend_img}
+                      className={recommend_section.sp_recommend_img}
                     />
                   </li>
                 </ul>
 
-                <ul className={RecommendSection.sp_recommend_img_list2}>
+                <ul className={recommend_section.sp_recommend_img_list2}>
                   <li>
-                    <div className={RecommendSection.recommend_box}>
-                      <div className={RecommendSection.bg_black}>
+                    <div className={recommend_section.recommend_box}>
+                      <div className={recommend_section.bg_black}>
                         <p>就活でアピールできる実績がほしい</p>
                       </div>
-                      <div className={RecommendSection.recommend_text}>
+                      <div className={recommend_section.recommend_text}>
                         <p>
                           ・就職活動でエントリーシートに何を書いて良いかわからない
                         </p>
@@ -173,29 +173,29 @@ export default function Home() {
                     <img
                       src="/recommend_polygon_bottom.png"
                       alt=""
-                      className={RecommendSection.sp_recommend_polygon}
+                      className={recommend_section.sp_recommend_polygon}
                     />
                   </li>
                   <li>
                     <img
                       src="/recommend_image_2.png"
                       alt=""
-                      className={RecommendSection.sp_recommend_img}
+                      className={recommend_section.sp_recommend_img}
                     />
                   </li>
                 </ul>
 
-                <ul className={RecommendSection.sp_recommend_img_list3}>
+                <ul className={recommend_section.sp_recommend_img_list3}>
                   <li>
-                    <div className={RecommendSection.recommend_box}>
-                      <div className={RecommendSection.bg_black}>
+                    <div className={recommend_section.recommend_box}>
+                      <div className={recommend_section.bg_black}>
                         <p>
                           エンジニアになりたいけど
                           <br />
                           何をしていいかわからない
                         </p>
                       </div>
-                      <div className={RecommendSection.recommend_text}>
+                      <div className={recommend_section.recommend_text}>
                         <p>
                           ・独学で挫折してしまい何から手をつけたら良いかわからない
                         </p>
@@ -207,14 +207,14 @@ export default function Home() {
                     <img
                       src="/recommend_polygon_bottom.png"
                       alt=""
-                      className={RecommendSection.sp_recommend_polygon}
+                      className={recommend_section.sp_recommend_polygon}
                     />
                   </li>
                   <li>
                     <img
                       src="/recommend_image_3.png"
                       alt=""
-                      className={RecommendSection.sp_recommend_img}
+                      className={recommend_section.sp_recommend_img}
                     />
                   </li>
                 </ul>
