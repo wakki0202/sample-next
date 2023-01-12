e33dc4e9c646aceba2bd23164ec89db4bd34d699
wakki <ytk0202@docomo.ne.jp>
2023-01-12 10:53:04 +0900

    fix クラス名をスネークケースに変更、css修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,22 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Top.module.scss";
 import LayoutNew from "../layouts/LayoutNew/";
-import IntroSection from "../styles/scss/module/IntroSection.module.scss";
+import intro_section from "../styles/scss/module/intro_section.module.scss";
 
 export default function Home() {
   return (
     <LayoutNew header={<></>} footer={<></>}>
       <div>
-        <section className="IntroSection">
-          <div className={IntroSection.intro_box}>
+        <section className="intro_section">
+          <div className={intro_section.intro_box}>
             <img
               src="/intro_img.png"
               alt=""
-              className={IntroSection.pc_intro_img}
+              className={intro_section.pc_intro_img}
             />
-            <img
-              src="/intro_img.png"
-              alt=""
-              className={IntroSection.sp_intro_img}
-            />
-            <p className={IntroSection.achivement}>
+            <p className={intro_section.achivement}>
               ※ DMM WEBCAMPで所定の学習および転職活動を
-              <br className={IntroSection.sp_br} />
+              <br className={intro_section.sp_br} />
               履行された方の実績（2022年7月時点）
             </p>
           </div>
